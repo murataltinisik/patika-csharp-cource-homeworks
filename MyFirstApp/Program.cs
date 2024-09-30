adc76c7efaf00566bd88a76404efdb218bc4cf13
Murat Altınışık <112017427+murataltinisik@users.noreply.github.com>
2024-09-30 21:02:22 +0300

    Commit

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -7,6 +7,7 @@ namespace MyFirstApp
         static void Main(string[] args)
         {
             Console.WriteLine("Murat ALTINIŞIK");
+            Console.WriteLine("Patika'nın uzman kadrosu ve tecrübeli eğitmeneleri sayesinde yazılım dünyasına hızlı bir şekilde giriş yapabileceğim için bu bootcampteyim!");
         }
     }
 }
