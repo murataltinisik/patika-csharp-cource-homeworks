c26a2d0282ac0fc5a52b4a9f9389a391d10b43cb
Murat Altınışık <112017427+murataltinisik@users.noreply.github.com>
2024-10-25 20:39:45 +0300

    Edited homeword

--- Week5Project/_02Project/Program.cs
+++ Week5Project/_02Project/Program.cs
@@ -1,17 +1,25 @@
-﻿// Define List
+﻿// Start
+Console.WriteLine("Program'dan çıkış yapmak için (q) basın");
+Console.WriteLine();
+
+// Define List
 List<string> guests = new List<string>();
 
-// Add item to List
-guests.Add("Bülent Ersoy");
-guests.Add("Ajda Pekkan");
-guests.Add("Ebru Gündeş");
-guests.Add("Hadise");
-guests.Add("Hande Yener");
-guests.Add("Tarkan");
-guests.Add("Funda Arar");
-guests.Add("Demet Akalın");
+// Get Values and push array
+do
+{
+    // Get Value
+    Console.Write("Please enter a guest name: ");
+    string input = Console.ReadLine();
+
+    if (input == "q") break;
+    
+    // Add item to list
+    guests.Add(input);
+} while (true);
 
 // Write Console
+Console.WriteLine();
 Console.WriteLine("------- DAVETLİLER -------");
 Console.WriteLine("--------------------------");
 for (int i = 0; i < guests.Count; i++) 
--- Week5Project/_02Project/obj/Debug/net8.0/_02Project.AssemblyInfo.cs
+++ Week5Project/_02Project/obj/Debug/net8.0/_02Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("_02Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67cf509340dbbb073e6b1eff4deab2e7d72df097")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee1e75dca2f2f3e2a6e620277dde1bf42ee6b12d")]
 [assembly: System.Reflection.AssemblyProductAttribute("_02Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("_02Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
