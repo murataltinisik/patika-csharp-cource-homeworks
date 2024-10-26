a8b087b208bac69e5c18ff6d1ee42b9b5d1af695
Murat Altınışık <112017427+murataltinisik@users.noreply.github.com>
2024-10-26 21:22:25 +0300

    Updated

--- Week5Project/_05Project/Program.cs
+++ Week5Project/_05Project/Program.cs
@@ -7,8 +7,12 @@
     // Write Console
     Console.WriteLine("Result: {0}", number * number);
 }
-catch (Exception ex)
+catch (FormatException ex)
 {
     Console.WriteLine("Invalid Input, please try again.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
 }
 
