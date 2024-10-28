a9b4d58be566534285082e141449bd2b53d5e549
Murat Altınışık <112017427+murataltinisik@users.noreply.github.com>
2024-10-28 18:28:00 +0300

    Homework 4

--- WEEK6/Homework4.sql
+++ WEEK6/Homework4.sql
@@ -3,3 +3,15 @@ SELECT DISTINCT replacement_cost FROM film;
 
 -- 2. Question
 SELECT COUNT(DISTINCT replacement_cost) FROM film;
+
+-- 3. Question
+SELECT COUNT(*) FROM film WHERE 
+	title LIKE 'T%'
+	AND
+	rating = 'G';
+
+-- 4. Question
+SELECT COUNT(*) FROM country WHERE LENGTH(country) = 5;
+
+-- 5. Question
+SELECT * FROM city WHERE city ILIKE '%r';
